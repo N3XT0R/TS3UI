e00023aee7248ae7a73b7f0c25a3c7acf94f31af
N3X <info@php-dev.info>
2015-10-13 23:57:18 +0200

    implemented form setter and getter methods into server service class.

--- module/Server/src/Server/Service/ServerService.php
+++ module/Server/src/Server/Service/ServerService.php
@@ -4,7 +4,7 @@
  * @author         N3X
  * @copyright      Copyright (c) 2015, Ilya Beliaev
  * @since          Version 1.0
- * 
+ *
  * $Id$
  * $Date$
  */
@@ -13,13 +13,14 @@ namespace Server\Service;
 
 use Zend\EventManager\EventManagerAwareInterface;
 use Zend\EventManager\EventManagerInterface;
+use Zend\Form\FormInterface;
 
 class ServerService implements EventManagerAwareInterface{
-    
+
     protected $aForms = array();
     protected $aMessages = array();#
     protected $oEventManager;
-    
+
     public function getEventManager() {
         return $this->oEventManager;
     }
@@ -29,40 +30,55 @@ class ServerService implements EventManagerAwareInterface{
         $this->oEventManager = $eventManager;
         return $this;
     }
-    
-    
+
+
     public function addMessage($sType, $sMessage){
         $this->aMessages[$sType] = $sMessage;
         return $this;
     }
-    
+
     public function getMessages(){
         return $this->aMessages;
     }
-    
+
     public function setMessages(array $aMessages){
         $this->aMessages = $aMessages;
         return $this;
     }
-    
+
     public function clearMessages(){
         $this->aMessages = array();
         return $this;
     }
-    
-    public function setForm($sName, $oForm){
+
+    /**
+     * set Form
+     * @param string $sName
+     * @param FormInterface $oForm
+     * @return \Server\Service\ServerService
+     */
+    public function setForm($sName, FormInterface $oForm){
         $this->aForms[$sName] = $oForm;
         return $this;
     }
-    
-    public function getForm(){
-        
-    }
-    
-    public function save(array $data){
-        
+
+    /**
+     * get Form
+     * @param string $sType
+     * @return \Zend\Form\FormInterface
+     */
+    public function getForm($sType){
+        $oForm = null;
+        if(array_key_exists($sType, $this->aForms)){
+            $oForm = $this->aForms[$sType];
+        }
+        return $oForm;
     }
 
-    
+    public function save(array $data){
+
+    }
+
+
 
 }
