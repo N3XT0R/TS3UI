5f464a6754c55eada29a8560542e88b561ef81d6
N3X <info@php-dev.info>
2015-10-29 23:27:57 +0100

    languages vars added

--- module/Server/language/de.php
+++ module/Server/language/de.php
@@ -10,8 +10,14 @@
  */
 
 return array(
-    'SERVER'                => 'Server',
-    'SERVER_SUB_LIST'       => 'Server verwalten',
-    'SERVER_SUB_CREATE'     => 'Server anlegen',
+    'SERVER'                        => 'Server',
+    'SERVER_SUB_LIST'               => 'Server verwalten',
+    'SERVER_SUB_CREATE'             => 'Server anlegen',
+    'SERVER_CREATE_INFO'            => '',
+    'SERVER_FORM_USERNAME'          => 'ServerQuery-User',
+    'SERVER_FORM_PASSWORD'          => 'Passwort',
+    'SERVER_FORM_HOSTNAME'          => 'IP-Adresse/Hostname',
+    'SERVER_FORM_PORT'              => 'Query-Port',
+    'SERVER_FORM_SAVE'              => 'Speichern',
 );
 
