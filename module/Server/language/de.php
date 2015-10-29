84b443635be10b0a413879a00f71e120681f10dc
N3X <info@php-dev.info>
2015-10-29 23:52:47 +0100

    added lang var for server creation.

--- module/Server/language/de.php
+++ module/Server/language/de.php
@@ -13,7 +13,7 @@ return array(
     'SERVER'                        => 'Server',
     'SERVER_SUB_LIST'               => 'Server verwalten',
     'SERVER_SUB_CREATE'             => 'Server anlegen',
-    'SERVER_CREATE_INFO'            => '',
+    'SERVER_CREATE_INFO'            => 'In diesem Schritt können neue dedizierte Server hinzugefügt werden. Die hier erwarteten Zugangsdaten sind die ServerQuery-Daten.',
     'SERVER_FORM_USERNAME'          => 'ServerQuery-User',
     'SERVER_FORM_PASSWORD'          => 'Passwort',
     'SERVER_FORM_HOSTNAME'          => 'IP-Adresse/Hostname',
